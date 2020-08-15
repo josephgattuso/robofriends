6253049ceed9d9d077c57964857edde616e6300b
josephgattuso <joseph.gattuso@gmail.com>
2020-08-15 15:34:57 +0100

    Updates

---
+++ precache-manifest.dbcaff62586450fd7a8e513fd6d1fad4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7305c470f6e4220f69ef7e25e65eb3c2",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "618464514e44820db587",
+    "url": "/robofriends/static/css/main.9ee37dd7.chunk.css"
+  },
+  {
+    "revision": "cadb83d287e325f91aec",
+    "url": "/robofriends/static/js/2.fa1425df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.fa1425df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "618464514e44820db587",
+    "url": "/robofriends/static/js/main.bb749839.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
