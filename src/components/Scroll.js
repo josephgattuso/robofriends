466668ecaf3789ed62f96fb0c3b392d656eeaee5
josephgattuso <joseph.gattuso@gmail.com>
2020-08-15 14:56:30 +0100

    update styles

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,13 @@ import React from "react";
 const Scroll = props => {
   return (
     <div
-      style={{ overflow: "scroll", border: "5px solid black", height: "800px" }}
+      style={{
+        overflow: "scroll",
+        borderTop: "10px solid #ff41b4",
+        height: "80vh",
+        margin: "5vh 0",
+        padding: "5vh 0",
+      }}
     >
       {props.children}
     </div>
