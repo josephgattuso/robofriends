51b1a255ab2bd79859a836f206a9ce2fee3646ed
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-09-26 07:32:57 +0100

    Updates

--- precache-manifest.1b9b5d5f2c901fe6c4946f23d4b3e242.js
+++ precache-manifest.1b9b5d5f2c901fe6c4946f23d4b3e242.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7f46dd5c60337ea8f376774f424cbd56",
+    "revision": "2b86d59a98a0bce1e0e6e72a3e186c6a",
     "url": "/robofriends/index.html"
   },
   {
-    "revision": "939c29a12718c22b5acf",
+    "revision": "abcf402c9619777cd68e",
     "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
   },
   {
-    "revision": "531117aca6101de9e925",
+    "revision": "dc926a6bf1f4ad61a775",
     "url": "/robofriends/static/css/main.b770996e.chunk.css"
   },
   {
-    "revision": "939c29a12718c22b5acf",
-    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js"
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "531117aca6101de9e925",
-    "url": "/robofriends/static/js/main.650109ce.chunk.js"
+    "revision": "dc926a6bf1f4ad61a775",
+    "url": "/robofriends/static/js/main.8763ec3f.chunk.js"
   },
   {
     "revision": "e7d2cbb368e0813da61d",
