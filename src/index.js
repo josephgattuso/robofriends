c849face9420397e6f51b8bf630a7ba62bd3ab23
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-09-26 11:46:25 +0100

    add redux store and provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import { searchRobots } from './reducers';
+
+import './index.css';
+
+const store = createStore(searchRobots);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
